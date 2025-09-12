b82deb5560f0b8f37d5db4dda9f1211088603575
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-12 20:32:20 +0530

    new files

---
+++ week1/hello app/hello app.swift
@@ -0,0 +1,18 @@
+//
+//  hello app.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 12/09/25.
+//
+
+import SwiftUI
+
+struct hello_app: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    hello_app()
+}
---
+++ week1/profile screen/profile.swift
@@ -0,0 +1,18 @@
+//
+//  profile.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 12/09/25.
+//
+
+import SwiftUI
+
+struct profile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profile()
+}
