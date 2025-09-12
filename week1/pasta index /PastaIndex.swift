3672be1af873b72def6e9bec412859eb91801059
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-13 00:08:54 +0530

    new apps

---
+++ week1/pasta index /PastaIndex.swift
@@ -0,0 +1,18 @@
+//
+//  PastaIndex.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 12/09/25.
+//
+
+import SwiftUI
+
+struct PastaIndex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PastaIndex()
+}
