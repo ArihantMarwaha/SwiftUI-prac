42e2306482b74265cb5f0d27d3ad05739eb8f6f7
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-12 10:13:51 +0530

    Initial Commit

---
+++ SwiftUI prac/SwiftUI_pracApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_pracApp.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_pracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
