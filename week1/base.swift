fa798f18f04e6836a77325195594598e3779f4a7
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-12 18:26:07 +0530

    new prac

---
+++ week1/base.swift
@@ -0,0 +1,18 @@
+//
+//  base.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 12/09/25.
+//
+
+import SwiftUI
+
+struct base: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    base()
+}
