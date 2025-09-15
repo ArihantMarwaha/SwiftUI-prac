8807e468f40d3f5014a9a613c9aaebf069635b6d
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-15 23:38:24 +0530

    aa

---
+++ observe.swift
@@ -0,0 +1,18 @@
+//
+//  observe.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 15/09/25.
+//
+
+import SwiftUI
+
+struct observe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    observe()
+}
