0aa9832628147b43e9e1bb96b92afbbff835e2a6
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-17 17:29:30 +0530

    reverse observeable

---
+++ week 2/resetcounter.swift
@@ -0,0 +1,18 @@
+//
+//  resetcounter.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 17/09/25.
+//
+
+import SwiftUI
+
+struct resetcounter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    resetcounter()
+}
