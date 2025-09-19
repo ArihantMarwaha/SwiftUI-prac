46455bcaebbcda379ab9c5e30817a2daa4b943ca
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-19 13:40:47 +0530

    challenge 3

---
+++ week 2/navigation /fruits/userchallenge.swift
@@ -0,0 +1,18 @@
+//
+//  userchallenge.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 19/09/25.
+//
+
+import SwiftUI
+
+struct userchallenge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    userchallenge()
+}
