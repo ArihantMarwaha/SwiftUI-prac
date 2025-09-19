5be7d9821124e56e7addc369732d7c2808b167ee
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-19 13:59:09 +0530

    challenge 4

---
+++ week 2/navigation /profiletesting.swift
@@ -0,0 +1,18 @@
+//
+//  profiletesting.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 19/09/25.
+//
+
+import SwiftUI
+
+struct profiletesting: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    profiletesting()
+}
