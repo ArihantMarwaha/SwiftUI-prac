53b09c0643e6496d590ffe0881909afcfe451e92
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-19 13:24:56 +0530

    challenge 2

---
+++ week 2/navigation /fruits/fruitdetails.swift
@@ -0,0 +1,18 @@
+//
+//  fruitdetails.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 19/09/25.
+//
+
+import SwiftUI
+
+struct fruitdetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fruitdetails()
+}
---
+++ week 2/navigation /fruits/fruitgenerate.swift
@@ -0,0 +1,18 @@
+//
+//  fruitgenerate.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 19/09/25.
+//
+
+import SwiftUI
+
+struct fruitgenerate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    fruitgenerate()
+}
