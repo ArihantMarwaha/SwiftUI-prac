ae30f59da6839c12bff334aadbb66681ab3313bb
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-20 23:04:07 +0530

    new challenge

---
+++ week 2/navigation /fruits/multicategory .swift
@@ -0,0 +1,18 @@
+//
+//  multicategory .swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 19/09/25.
+//
+
+import SwiftUI
+
+struct multicategory_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    multicategory_()
+}
