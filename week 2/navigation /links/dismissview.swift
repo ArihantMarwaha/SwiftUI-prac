905a2647d2a207cc611599fd4c769ea0133f2778
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-21 23:27:13 +0530

    challenges 5 and 6

---
+++ week 2/navigation /links/dismissview.swift
@@ -0,0 +1,18 @@
+//
+//  dismissview.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 21/09/25.
+//
+
+import SwiftUI
+
+struct dismissview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    dismissview()
+}
---
+++ week 2/navigation /links/multistepnavigation.swift
@@ -0,0 +1,18 @@
+//
+//  multistepnavigation.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 21/09/25.
+//
+
+import SwiftUI
+
+struct multistepnavigation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    multistepnavigation()
+}
