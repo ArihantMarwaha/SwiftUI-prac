2c03411a182caab2d7e827988bac6eefcad66e76
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-22 13:43:00 +0530

    notes staged

--- week 2/notes/notesapp.swift
+++ week 2/notes/notesapp.swift
@@ -7,10 +7,91 @@
 
 import SwiftUI
 
-struct notesapp: View {
+
+struct notelist:Identifiable {
+    
+    var id: UUID = UUID()
+    var title : String
+    var notedecription : String
+    
+}
+
+struct noteEditView : View {
+    @Environment(\.dismiss) var dismiss
+    @Binding var toast : notelist
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Form{
+            
+            TextField("Title",text: $toast.title)
+            TextField("Description",text: $toast.notedecription)
+            
+        }
+        .toolbar{
+            ToolbarItem(placement: .navigationBarTrailing) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Text("Dismiss")
+                }
+            }
+        }
+        
     }
+}
+
+struct notedesc : View {
+    
+    @Binding var toast : notelist
+    
+    var body: some View {
+        
+        
+        VStack{
+            Text("\(toast.notedecription)")
+                .font(.largeTitle)
+                .bold()
+                .fontWidth(.expanded)
+            
+        }
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                NavigationLink("Edit"){
+                    noteEditView(toast: $toast)
+                }
+            }
+            
+            
+        }
+        .navigationTitle(toast.title)
+        .navigationBarTitleDisplayMode(.large)
+    }
+    
+}
+
+struct notesapp: View {
+    
+    @State var notelisted : [notelist]  = [
+        notelist(title: "Title 1", notedecription: "This is the decription of the note 1 "),
+        
+        notelist(title: "Title 2", notedecription: "This is the decription of the note 1 "),
+        
+        notelist(title: "Title 3", notedecription: "This is the decription of the note 1 ")
+        
+    ]
+    
+    var body: some View {
+        NavigationStack{
+            List($notelisted){$item in
+                NavigationLink(item.title,destination: notedesc(toast: $item))
+            }
+            .navigationTitle("Notes")
+        }
+        
+        
+    }
+
 }
 
 #Preview {
