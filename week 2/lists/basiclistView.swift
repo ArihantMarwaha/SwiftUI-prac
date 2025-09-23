dde2df5a97c96d0e0dbffad8b5c6b6e83b830512
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-24 00:51:03 +0530

    lists

---
+++ week 2/lists/basiclistView.swift
@@ -0,0 +1,18 @@
+//
+//  basiclistView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 23/09/25.
+//
+
+import SwiftUI
+
+struct basiclistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    basiclistView()
+}
---
+++ week 2/modifiers /modiferBasicsView.swift
@@ -0,0 +1,18 @@
+//
+//  modiferBasicsView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 23/09/25.
+//
+
+import SwiftUI
+
+struct modiferBasicsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    modiferBasicsView()
+}
