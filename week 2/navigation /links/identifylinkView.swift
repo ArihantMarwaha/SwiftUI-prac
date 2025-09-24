a4af5cea6a68deee74913612ed508f653f47e367
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-24 23:46:58 +0530

    stage

---
+++ week 2/navigation /links/identifylinkView.swift
@@ -0,0 +1,18 @@
+//
+//  identifylinkView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 24/09/25.
+//
+
+import SwiftUI
+
+struct identifylinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    identifylinkView()
+}
