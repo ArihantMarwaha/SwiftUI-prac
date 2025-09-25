6b7f5c2ff68f3105e6169bb8fde9963ff96ddc69
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-25 20:32:01 +0530

    sheets

---
+++ week 2/navigation /presentation /itemsheet.swift
@@ -0,0 +1,18 @@
+//
+//  itemsheet.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 25/09/25.
+//
+
+import SwiftUI
+
+struct itemsheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    itemsheet()
+}
---
+++ week 2/navigation /presentation /sheet.swift
@@ -0,0 +1,18 @@
+//
+//  sheet.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 25/09/25.
+//
+
+import SwiftUI
+
+struct sheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    sheet()
+}
