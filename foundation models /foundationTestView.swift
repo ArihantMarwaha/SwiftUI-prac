a9e0bf6cb89d1b1c8a6e4013f85dbbe6bbdbf176
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-27 20:44:43 +0530

    check boards stage

--- foundation models /foundationTestView.swift
+++ foundation models /foundationTestView.swift
@@ -5,14 +5,33 @@
 //  Created by Arihant Marwaha on 26/09/25.
 //
 
-import SwiftUI
+import FoundationModels
+import Playgrounds
+import Foundation
 
-struct foundationTestView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+
+
+#Playground {
+    
+    let instructions = "your job is to create a structured itenary for the user. and. this making a ddetailed list of places to visit and why you should visit them. Include resturants and places to visit and  Always include a title , a little descrtiption and a day by day plan"
+    
+    let session = LanguageModelSession(instructions: instructions)
+    
+    let response = try await session.respond(to:"Generate a 3 day itenary for italy")
+    
 }
 
-#Preview {
-    foundationTestView()
+
+#Playground{
+    
+    let ses = SystemLanguageModel.default
+    
+    switch ses.availability {
+    case .available:
+        print("the model is available")
+    default:
+        print("The model is not responisive")
+        
+    }
+    
 }
--- week 3/grids/recursiveCheckBoard.swift
+++ week 3/grids/recursiveCheckBoard.swift
@@ -7,10 +7,74 @@
 
 import SwiftUI
 
+
+struct Checkerboard: View {
+    var size: Int = 2
+    var depth: Int = 1
+    
+    var body: some View {
+        Grid {
+            ForEach(0..<size, id: \.self) { row in
+                GridRow {
+                    ForEach(0..<size, id: \.self) { col in
+                        cellAt(row: row, col: col)
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func cellAt(row: Int, col: Int) -> some View {
+        let isDark = (row + col).isMultiple(of: 2)
+        
+        Rectangle()
+            .fill(isDark ? .black : .white)
+            .frame(width: 40, height: 40)
+    }
+}
+
+struct RecursiveCheckerboard: View {
+    var size: Int = 4
+    var depth: Int = 2   // controls recursion depth
+    
+    var body: some View {
+        Grid {
+            ForEach(0..<size, id: \.self) { row in
+                GridRow {
+                    ForEach(0..<size, id: \.self) { col in
+                        cellAt(row: row, col: col)
+                    }
+                }
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func cellAt(row: Int, col: Int) -> some View {
+        let isDark = (row + col).isMultiple(of: 2)
+        
+        if depth > 1 {
+            RecursiveCheckerboard(size: size, depth: depth - 1)
+                .frame(width: 40, height: 40) // scale down subgrid
+        } else {
+            Rectangle()
+                .fill(isDark ? .black : .white)
+                .frame(width: 40, height: 40)
+        }
+    }
+}
+
+
+
 struct recursiveCheckBoard: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+            ScrollView {
+                VStack {
+                    RecursiveCheckerboard(size: 2, depth: 2) // just 4x4// deeper recursion
+                }
+            }
+        }
 }
 
 #Preview {
