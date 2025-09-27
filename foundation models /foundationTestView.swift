fdcc51b46840274f7b6114622b4378389b1bfaab
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-27 20:44:21 +0530

    recursive check board

---
+++ foundation models /foundationTestView.swift
@@ -0,0 +1,18 @@
+//
+//  foundationTestView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 26/09/25.
+//
+
+import SwiftUI
+
+struct foundationTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    foundationTestView()
+}
---
+++ week 3/grids/recursiveCheckBoard.swift
@@ -0,0 +1,18 @@
+//
+//  recursiveCheckBoard.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 26/09/25.
+//
+
+import SwiftUI
+
+struct recursiveCheckBoard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    recursiveCheckBoard()
+}
