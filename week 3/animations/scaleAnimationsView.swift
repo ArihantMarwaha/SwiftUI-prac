01c69ebbd1c6c2a169c7cab193acaf9589d0fb19
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-28 19:17:12 +0530

    animations

---
+++ week 3/animations/scaleAnimationsView.swift
@@ -0,0 +1,18 @@
+//
+//  scaleAnimationsView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 28/09/25.
+//
+
+import SwiftUI
+
+struct scaleAnimationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    scaleAnimationsView()
+}
