6e65fe782ed346d3146c2c6718f70175fe584b42
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-29 23:15:14 +0530

    push

---
+++ week 3/animations/CircleGrowView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleGrowView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 29/09/25.
+//
+
+import SwiftUI
+
+struct CircleGrowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleGrowView()
+}
