542c33de2c425b9217222b5e73ef6a06128c5184
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-09-30 18:44:02 +0530

    new stage

---
+++ week 3/grids/RecursiveGrid.swift
@@ -0,0 +1,18 @@
+//
+//  RecursiveGrid.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 30/09/25.
+//
+
+import SwiftUI
+
+struct RecursiveGrid: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecursiveGrid()
+}
