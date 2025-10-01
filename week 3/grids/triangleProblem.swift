537709917914d1cf43ffff2bf6363ce58120a65a
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-01 22:49:13 +0530

    somit

---
+++ week 3/grids/triangleProblem.swift
@@ -0,0 +1,18 @@
+//
+//  triangleProblem.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 01/10/25.
+//
+
+import SwiftUI
+
+struct triangleProblem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    triangleProblem()
+}
