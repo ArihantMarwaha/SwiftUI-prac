2f57afe1b69c6153e40ad3c51a15ee7b1c0eebaa
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-02 19:59:46 +0530

    commit

---
+++ week 3/grids/mazeView.swift
@@ -0,0 +1,18 @@
+//
+//  mazeView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 02/10/25.
+//
+
+import SwiftUI
+
+struct mazeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    mazeView()
+}
