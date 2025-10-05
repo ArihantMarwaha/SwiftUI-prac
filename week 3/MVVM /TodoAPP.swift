42774e227b5d3ee379b7810ea93aa288ea22cae9
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-05 14:22:05 +0530

    new list item

---
+++ week 3/MVVM /TodoAPP.swift
@@ -0,0 +1,18 @@
+//
+//  TodoAPP.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 05/10/25.
+//
+
+import SwiftUI
+
+struct TodoAPP: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoAPP()
+}
