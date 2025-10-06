678e889842010a3c537b0c492a08d111c80a57f7
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-06 15:42:51 +0530

    commit

---
+++ week 4/Network Self .swift
@@ -0,0 +1,18 @@
+//
+//  Network Self .swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 06/10/25.
+//
+
+import SwiftUI
+
+struct Network_Self_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Network_Self_()
+}
