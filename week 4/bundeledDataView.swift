e574540172958efe639f73b0415af548863e7121
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-07 20:01:26 +0530

    networking nrw code

---
+++ week 4/bundeledDataView.swift
@@ -0,0 +1,18 @@
+//
+//  bundeledDataView.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 07/10/25.
+//
+
+import SwiftUI
+
+struct bundeledDataView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    bundeledDataView()
+}
