be2d5c067de110ec2fdafa552561700a8fd0bdc7
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-09 19:09:50 +0530

    ok

---
+++ week 4/DELETE.swift
@@ -0,0 +1,18 @@
+//
+//  DELETE.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 09/10/25.
+//
+
+import SwiftUI
+
+struct DELETE: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DELETE()
+}
---
+++ week 4/POSTandDELETE .swift
@@ -0,0 +1,18 @@
+//
+//  POSTandDELETE .swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 09/10/25.
+//
+
+import SwiftUI
+
+struct POSTandDELETE_: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    POSTandDELETE_()
+}
