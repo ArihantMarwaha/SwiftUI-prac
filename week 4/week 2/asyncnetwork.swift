88cebeb76d2857e3b56ec41722a4301b64eedbf1
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-18 15:52:20 +0530

    ok

--- week 4/week 2/asyncnetwork.swift
+++ week 4/week 2/asyncnetwork.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+struct nroduct: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let image: String
+}
+
