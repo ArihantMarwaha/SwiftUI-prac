e4e04b09b5ece3fc97ad48b4a972c80dac2203f3
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-10-26 22:06:02 +0530

    completed all medium

--- interview questions/question 1.swift
+++ interview questions/question 1.swift
@@ -239,14 +239,144 @@ Given an array of integers, find the two numbers that sum to a target.
 
  
 Write a function to flatten a nested array (recursion).
+ func flattenManual(_ input: [Any]) -> [Any] {
+     var res: [Any] = []
+     var i = 0
+     while i < input.count {
+         if let sub = input[i] as? [Any] {
+             let flat = flattenManual(sub)
+             var j = 0
+             while j < flat.count {
+                 res.append(flat[j])
+                 j += 1
+             }
+         } else {
+             res.append(input[i])
+         }
+         i += 1
+     }
+     return res
+ }
+ flattenManual([1,[2,[3,4]],5])
 
 Implement a singly linked list in Swift with insert, delete, and print functions.
+ class Node {
+     var val: Int
+     var next: Node?
+     init(_ v: Int) { val = v }
+ }
+
+ class LinkedList {
+     var head: Node?
+     
+     func insert(_ v: Int) {
+         let node = Node(v)
+         if head == nil { head = node; return }
+         var curr = head
+         while curr?.next != nil { curr = curr?.next }
+         curr?.next = node
+     }
+     
+     func delete(_ v: Int) {
+         guard var curr = head else { return }
+         if curr.val == v { head = curr.next; return }
+         while let next = curr.next {
+             if next.val == v {
+                 curr.next = next.next
+                 return
+             }
+             curr = next
+         }
+     }
+     
+     func printList() {
+         var curr = head
+         while curr != nil {
+             print(curr!.val, terminator:" ")
+             curr = curr!.next
+         }
+         print()
+     }
+ }
+
+ let l = LinkedList()
+ l.insert(1); l.insert(2); l.insert(3)
+ l.delete(2)
+ l.printList() // 1 3
+
 
 Find the intersection of two arrays.
+ func intersectionManual(_ a: [Int], _ b: [Int]) -> [Int] {
+     var result: [Int] = []
+     var i = 0
+     while i < a.count {
+         var j = 0
+         var found = false
+         while j < b.count {
+             if a[i] == b[j] {
+                 // check duplicate in result
+                 var k = 0
+                 var dup = false
+                 while k < result.count {
+                     if result[k] == a[i] { dup = true; break }
+                     k += 1
+                 }
+                 if !dup { result.append(a[i]) }
+                 found = true
+                 break
+             }
+             j += 1
+         }
+         i += 1
+     }
+     return result
+ }
+ intersectionManual([1,2,3,4],[3,4,5]) // [3,4]
 
+ 
 Implement quicksort or mergesort.
+ func mergeSortManual(_ a: [Int]) -> [Int] {
+     let n = a.count
+     if n <= 1 { return a }
+     let mid = n / 2
+     var left: [Int] = []
+     var right: [Int] = []
+     var i = 0
+     while i < mid { left.append(a[i]); i += 1 }
+     while i < n { right.append(a[i]); i += 1 }
+     
+     let sortedL = mergeSortManual(left)
+     let sortedR = mergeSortManual(right)
+     return mergeManual(sortedL, sortedR)
+ }
 
+ func mergeManual(_ L: [Int], _ R: [Int]) -> [Int] {
+     var res: [Int] = []
+     var i = 0, j = 0
+     while i < L.count && j < R.count {
+         if L[i] < R[j] { res.append(L[i]); i += 1 }
+         else { res.append(R[j]); j += 1 }
+     }
+     while i < L.count { res.append(L[i]); i += 1 }
+     while j < R.count { res.append(R[j]); j += 1 }
+     return res
+ }
+ mergeSortManual([4,1,3,9,7]) // [1,3,4,7,9]
+
+ 
 Compute nth Fibonacci number recursively with memoization.
+ func fibMemoManual(_ n: Int, _ memo: inout [Int:Int]) -> Int {
+     if n <= 1 { return n }
+     if let val = memo[n] { return val }
+     let first = fibMemoManual(n-1, &memo)
+     let second = fibMemoManual(n-2, &memo)
+     let val = first + second
+     memo[n] = val
+     return val
+ }
+var m: [Int:Int] = [:]
+fibMemoManual(10, &m) // 55
+
 
 Hard
 
