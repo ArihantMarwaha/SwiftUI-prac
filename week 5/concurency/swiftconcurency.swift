21980ba4a15bb507511c0967efca573bb879991f
Arihant Marwaha <arihantmarwaha@icloud.com>
2025-11-10 22:04:54 +0530

    concurency

---
+++ week 5/concurency/swiftconcurency.swift
@@ -0,0 +1,18 @@
+//
+//  swiftconcurency.swift
+//  SwiftUI prac
+//
+//  Created by Arihant Marwaha on 10/11/25.
+//
+
+import SwiftUI
+
+struct swiftconcurency: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    swiftconcurency()
+}
